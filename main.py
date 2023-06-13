1b45c166ea775f81144bfa10f4d9f10b4512e9cb
Romina <escudero.rominasoledad@gmail.com>
2023-06-13 11:14:29 -0300

    actualizacion de main.py.

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ estrenos_por_dia = pd.read_csv("data/estrenos_por_dia.csv", sep=",")
 movies = pd.read_csv("data/movies_etl.csv", sep=",")
 actors = pd.read_csv("data/actors.csv", sep=",")
 directors = pd.read_csv("data/directors.csv", sep=",")
+df = pd.read_csv("data/ml_df.csv", sep=",")
+pelis = movies[['title','release_year','vote_count','vote_average']][movies['vote_count'] > 2000]
+
 
 # Funciones
 
@@ -88,38 +91,34 @@ def score_titulo( titulo_de_la_filmación ):
 #4
 @app.get('/votos_titulo/{titulo}')
 def votos_titulo( titulo_de_la_filmación ): 
-
-    # REVISAR .str.contains!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
     ''' Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor promedio de las votaciones. 
     La misma variable deberá de contar con al menos 2000 valoraciones, 
     caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, no se devuelve ningun valor.
     Ejemplo de retorno: La película X fue estrenada en el año X. La misma cuenta con un total de X valoraciones, con un promedio de X
     '''
-    pelis = (
-        movies[movies['title']
-        == titulo_de_la_filmación.title().strip()]
-        [['title','release_year','vote_count','vote_average']]
-        )
 
-    out = []
-    
-    for index, row in pelis.iterrows():
-        title = row['title']
-        release_year = row['release_year']
-        vote_count = row['vote_count']
-        vote_average = row['vote_average']
+    pelis = movies[['title','release_year','vote_count','vote_average']][movies['vote_count'] > 2000]
 
-        if vote_count < 2000:
-            message = "La película {} fue estrenada en {}. La misma no cuenta con las valoraciones suficientes para calcular el valor promedio de las votaciones".format(title, release_year)
-        else:    
-            message = "La película {} fue estrenada en {}. La misma cuenta con un total de {} valoraciones, con un promedio de {}".format(title, release_year, vote_count,vote_average)
+    mensaje = "La pelicula {} no cuenta con las valoraciones suficientes para calcular el valor promedio de las votaciones".format(titulo_de_la_filmación)
+
+
+    if pelis['title'].str.contains(titulo_de_la_filmación).any():
+        pelis = pelis[pelis['title']==titulo_de_la_filmación]
+        anios = pelis['release_year'].tolist()
+        vote_count = pelis['vote_count'].tolist()
+        vote_average = pelis['vote_average'].tolist()
+  
+        data = {
+            'titulo':titulo_de_la_filmación, 
+            'anio': anios, 
+            'voto_total':vote_count, 
+            'voto_promedio':vote_average}
         
-        out.append(message)
-    
-    out= tuple(out)  
+    else:
+        data = {mensaje}
 
-    return {out}  
+
+    return data 
       
 
 
@@ -132,7 +131,7 @@ def get_actor( nombre_actor ):
     Ejemplo de retorno: El actor X ha participado de X cantidad de filmaciones, el mismo ha conseguido un retorno de X con un promedio de X por filmación
     '''
     
-    id_actor = actors[actors.name == nombre_actor.strip()].iloc[0,0]
+    id_actor = actors[actors.name == nombre_actor.title().strip()].iloc[0,0]
 
     df = (
         pd.merge(
@@ -152,7 +151,6 @@ def get_actor( nombre_actor ):
 
     return data
 
-
 #6
 @app.get('/get_director/{nombre_director}')
 def get_director( nombre_director ): 
@@ -160,7 +158,7 @@ def get_director( nombre_director ):
     Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
     Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.
     '''
-    id_director = directors[directors.name == nombre_director.strip()].iloc[0,0]
+    id_director = directors[directors.name == nombre_director.title().strip()].iloc[0,0]
 
     df = (
         pd.merge(
@@ -187,7 +185,6 @@ def get_director( nombre_director ):
         'revenue_pelicula':revenue_peliculas}
 
     return data
-
 # ML
 
 # Instanciamos el CV
